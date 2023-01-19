bed5b34a86a33c2cb83fa06ce5666ecad15090dd
Krystian Stopka <kr6stopka@gmail.com>
2023-01-19 21:19:33 +0100

    added app start

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { FunctionComponent } from "react";
+
+interface AppProps {}
+
+const App: FunctionComponent<AppProps> = () => {
+  return <div className="wrapper"></div>;
+};
+
+export default App;
